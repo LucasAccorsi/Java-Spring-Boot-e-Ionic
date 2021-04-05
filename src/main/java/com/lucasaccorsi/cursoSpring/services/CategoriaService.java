a1925ee3f18064f60d3eb94032381a3ed3e8d55b
Lucas Accorsi <lucasaccorsi@hotmail.com>
2021-04-05 17:20:02 -0300

    Criando repository e service para caregoria

---
+++ src/main/java/com/lucasaccorsi/cursoSpring/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.lucasaccorsi.cursoSpring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasaccorsi.cursoSpring.domain.Categoria;
+import com.lucasaccorsi.cursoSpring.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
