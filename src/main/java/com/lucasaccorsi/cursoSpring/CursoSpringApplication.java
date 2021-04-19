44741c588bcc42694ac10ab934f9bfacaf6f5486
Lucas Accorsi <lucasaccorsi@hotmail.com>
2021-04-19 16:56:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/lucasaccorsi/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/lucasaccorsi/cursoSpring/CursoSpringApplication.java
@@ -1,6 +1,5 @@
 package com.lucasaccorsi.cursoSpring;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucasaccorsi.cursoSpring.domain.Categoria;
+import com.lucasaccorsi.cursoSpring.domain.Produto;
 import com.lucasaccorsi.cursoSpring.repositories.CategoriaRepository;
+import com.lucasaccorsi.cursoSpring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -27,7 +31,18 @@ public class CursoSpringApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
-
 }
--- src/main/java/com/lucasaccorsi/cursoSpring/domain/Categoria.java
+++ src/main/java/com/lucasaccorsi/cursoSpring/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.lucasaccorsi.cursoSpring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -14,8 +17,12 @@ public class Categoria implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
-
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	
 	public Categoria() {}
 
@@ -46,6 +53,14 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -72,4 +87,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+	
 }
