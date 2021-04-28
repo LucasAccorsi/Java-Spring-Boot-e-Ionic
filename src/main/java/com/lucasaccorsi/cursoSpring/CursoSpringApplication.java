c727d27366708504b7bd465fdb7008f7c66ee008
Lucas Accorsi <lucasaccorsi@hotmail.com>
2021-04-28 18:49:48 -0300

    Estado e cidade

--- src/main/java/com/lucasaccorsi/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/lucasaccorsi/cursoSpring/CursoSpringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucasaccorsi.cursoSpring.domain.Categoria;
+import com.lucasaccorsi.cursoSpring.domain.Cidade;
+import com.lucasaccorsi.cursoSpring.domain.Estado;
 import com.lucasaccorsi.cursoSpring.domain.Produto;
 import com.lucasaccorsi.cursoSpring.repositories.CategoriaRepository;
+import com.lucasaccorsi.cursoSpring.repositories.CidadeRepository;
+import com.lucasaccorsi.cursoSpring.repositories.EstadoRepository;
 import com.lucasaccorsi.cursoSpring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursoSpringApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	
 	public static void main(String[] args) {
@@ -44,5 +52,18 @@ public class CursoSpringApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));	
 	}
 }
